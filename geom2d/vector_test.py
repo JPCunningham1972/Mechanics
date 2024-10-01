4c43029797fc37812f73e5ffcd1d9382f51b175e
Gnasch1972 <juiblex_ii@yahoo.com>
2024-09-30 20:52:47 -0700

    Adding vector tests

--- geom2d/vector_test.py
+++ geom2d/vector_test.py
@@ -1,6 +1,7 @@
 import unittest
 import math
 
+from geom2d.nums import are_close_enough
 from geom2d.vector import Vector
 
 class TestVector(unittest.TestCase):
@@ -17,11 +18,29 @@ class TestVector(unittest.TestCase):
         actual = self.u - self.v
         self.assertEqual(expected, actual)
 
+    def test_scaled_by(self):
+        expected = Vector(2, 4)
+        actual = self.u.scaled_by(2)
+        self.assertEqual(expected, actual)
+
     def test_norm(self):
         expected = 2.23606797749979
         actual = self.u.norm
         self.assertAlmostEqual(expected, actual)
 
     def test_is_normal(self):
+        w = Vector(1,1)
         self.assertFalse(self.u.is_normal)
+        self.assertTrue(w)
 
+    def test_sine(self):
+        w =Vector(math.sqrt(3)/2, 1/2)
+        expected = math.sin(math.pi / 6)
+        actual = w.sine
+        self.assertAlmostEqual(expected, actual)
+
+    def test_cosine(self):
+        w =Vector(math.sqrt(3)/2, 1/2)
+        expected = math.cos(math.pi / 6)
+        actual = w.cosine
+        self.assertAlmostEqual(expected, actual)
