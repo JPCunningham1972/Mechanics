fc4f4de6a4f1f22ad4ac4cd1c6f1ada12dc02f69
JPCunningham1972 <orcus963@gmail.com>
2024-10-04 21:52:28 -0700

    chore: commit comments for vectors.py

--- geom2d/vectors.py
+++ geom2d/vectors.py
@@ -2,10 +2,28 @@ from geom2d.point import Point
 from geom2d.vector import Vector
 
 def make_vector_between(p:Point, q: Point):
+    """
+    Creates a Vector from Point:p and Point:q
+    :param p: Point
+    :param q: Point
+    :return: Vector q - p
+    """
     return q - p
 
 def make_versor(u: float, v:float):
+    """
+    Creates a unit vector from two floats
+    :param u: float
+    :param v: float
+    :return: Vector - Unit Vector
+    """
     return Vector(u, v).normalized()
 
 def make_versor_between(p:Point, q: Point):
+    """
+    Creates a unit vector from Point:p and Point:q
+    :param p: Point
+    :param q: Point
+    :return: Vector - Unit Vector
+    """
     return make_vector_between(p, q).normalized()
