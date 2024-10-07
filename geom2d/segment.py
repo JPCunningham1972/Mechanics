55092c5821a15cdf991c6f05a9da9367777204f0
JPCunningham1972 <orcus963@gmail.com>
2024-10-06 21:26:34 -0700

    chore: finished the segment.py and test_segment.py

--- geom2d/segment.py
+++ geom2d/segment.py
@@ -3,32 +3,112 @@ from geom2d.vectors import make_vector_between, make_versor_between
 from geom2d import tparam
 
 class Segment:
+    """
+    Segment is a 2D segment defined by its start and end point
+    """
     def __init__(self, start: Point, end: Point):
         self.start = start
         self.end = end
 
     @property
     def direction_vector(self):
+        """
+         Creates a Vector from Segement:this
+        :return:Vector
+        """
         return make_vector_between(self.start, self.end)
 
     @property
     def direction_versor(self):
+        """
+        Creates unit vector from Segment:this
+        :return: Vector unit vector constructed from a Segment object
+        """
         return make_versor_between(self.start, self.end)
 
     @property
     def normal_versor(self):
+        """
+        Creates a Vector normal to Vector:this
+        :return: Vector normal vector constructed from a Segment object
+        """
         return self.direction_versor.perpendicular()
 
     @property
     def length(self):
+        """
+        Calculates the Euclidean distance from start to end of Segment:this
+        :return: float - sqrt(start**2 + end**2)
+        """
         return self.start.distance_to(self.end)
 
     @property
     def middle(self):
+        """
+        Create a Point which is the mid point of Segment:this
+        :return: Point - from midpoint formula
+        """
         return self.point_at(tparam.MIDDLE)
 
     def point_at(self, t: float):
+        """
+        Creates a Point at t * displacment
+        :param t: float (0 <= t <= 1)
+        :return: Point - from start to t * displacment
+        """
         tparam.ensure_valid(t)
         return self.start.displaced(self.direction_vector, t)
 
+    def closet_point_to(self, p: Point):
+        """
+        Calculates the nearest Point on Segment:this and create a Point
+        :param p: Point
+        :return: Point - nearest Point on Segment:this and a Point
+        """
+        v = make_vector_between(self.start, p)
+        d = self.direction_versor
+        vs = v.projection_over(d)
 
+        if vs < 0:
+            return self.start
+
+        if vs > self.length:
+            return self.end
+
+        return self.start.displaced(d, vs)
+
+    def distance_to(self, p: Point):
+        """
+        Calculates the Euclidean distance from closet point to p on Segment:this
+        and Point p
+        :param p: Point - point not on segment
+        :return: float - Euclidean Distance
+        """
+        return p.distance_to(self.closet_point_to(p))
+
+    def intersection_with(self, other):
+        d1, d2 = self.direction_vector, other.direction_vector
+
+        if d1.is_parallel_to(d2):
+            return None
+
+        cross_prod = d1.cross(d2)
+        delta = other.start - self.start
+        t1 = (delta.u * d2.v - delta.v * d2.u) / cross_prod
+        t2 = (delta.u * d1.v - delta.v * d1.u) / cross_prod
+
+        if tparam.is_valid(t1) and tparam.is_valid(t2):
+            return self.point_at(t1)
+
+        else:
+            return None
+
+    def __eq__(self, other):
+        if self is other:
+            return True
+        if not isinstance(other, Segment):
+            return False
+        return self.start == other.start and self.end == other.end
+
+    def __str__(self):
+        return f'segment {self.start} to {self.end}'
--- geom2d/segment_test.py
+++ geom2d/segment_test.py
@@ -1,5 +1,8 @@
 import math
 import unittest
+from math import expm1
+
+from pip._vendor.rich import segment
 
 from geom2d.point import Point
 from geom2d.segment import Segment
@@ -33,3 +36,71 @@ class TestSegment(unittest.TestCase):
         expected = Point(200, 200)
         actual = self.segment.middle
         self.assertEqual(expected, actual)
+
+    def test_closest_point_is_start(self):
+        p = Point(500, 20)
+        expected = self.start
+        actual = self.segment.closet_point_to(p)
+        self.assertEqual(expected, actual)
+
+    def test_closet_point_is_end(self):
+        p = Point(20, 500)
+        expected = self.end
+        actual = self.segment.closet_point_to(p)
+        self.assertEqual(expected, actual)
+
+    def test_closest_point_is_middle(self):
+        p = Point(250, 250)
+        expected = Point(200, 200)
+        actual = self.segment.closet_point_to(p)
+        self.assertEqual(expected, actual)
+
+    def test_distance_to_point_start(self):
+        p = Point(500, 20)
+        expected = 101.9803902718557
+        actual = self.segment.distance_to(p)
+        self.assertEqual(expected, actual)
+
+    def test_distance_to_point_end(self):
+        p = Point(20, 500)
+        expected = 101.9803902718557
+        actual = self.segment.distance_to(p)
+        self.assertAlmostEqual(expected, actual)
+
+    def test_distance_to_point_middle(self):
+        p = Point(250, 250)
+        expected = 70.71067811865476
+        actual = self.segment.distance_to(p)
+        self.assertAlmostEqual(expected, actual)
+
+    def test_parallel_segment_no_intersection(self):
+        other = Segment(Point(200, 0), Point(0,200))
+        actual = self.segment.intersection_with(other)
+        self.assertIsNone(actual)
+
+    def test_parallel_segment_NI_t1_ob(self):
+        other = Segment(Point(100, 0), Point(0, 200))
+        actual = self.segment.intersection_with(other)
+        self.assertIsNone(actual)
+
+    def test_parallel_segment_NI_t2_ob(self):
+        other = Segment(Point(200, 0), Point(0,200))
+        p = Segment(Point(400, 0), Point(0, 300))
+        actual = p.intersection_with(other)
+        self.assertIsNone(actual)
+
+    def test_segments_intersection(self):
+        other = Segment(Point(0,0), Point(400,400))
+        expected = Point(200, 200)
+        actual = self.segment.intersection_with(other)
+        self.assertEqual(expected, actual)
+
+    def test_equality(self):
+        segment2 = Segment(Point(200 ,0), Point(0, 200))
+        segment3 = Segment(Point(400,0), Point(0,400))
+        segment4 = 'the'
+        self.assertTrue(segment2 == segment2)
+        self.assertFalse(segment2 == segment4)
+        self.assertTrue(self.segment == segment3)
+        self.assertFalse(self.segment == segment2)
+
